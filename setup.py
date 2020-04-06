9d4cd66a6d4ccdc3fcb3623e96b4704c99701bd9
rsheeter <rsheeter@google.com>
2020-04-05 22:35:14 -0700

    Requirements ftw

--- setup.py
+++ setup.py
@@ -13,7 +13,19 @@ setup(
         ],
     },
 
-    install_requires=["lxml>=4.0", "skia-pathops>=0.3",],
+    install_requires=[
+        "absl-py>=0.9.0",
+        "fs==2.4.11",
+        "lxml>=4.0",
+        "regex>=2020.4.4",
+        "skia-pathops>=0.3",
+        "ufo2ft>=2.13.0",
+        "ufoLib2>=0.6.2",
+
+        # Horrible horrors lie here. Remove after merges.
+        "fontTools @ https://github.com/fonttools/fonttools/tarball/otdata-colr",
+        "nanosvg @ https://github.com/rsheeter/nanosvg/tarball/master",
+    ],
 
     # metadata to display on PyPI
     author="Rod S",
