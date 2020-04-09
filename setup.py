ce3959a0eb218d02bd2a24c16da8deafd180146b
rsheeter <rsheeter@google.com>
2020-04-09 12:23:13 -0700

    get nanosvg from pypi

--- setup.py
+++ setup.py
@@ -10,18 +10,19 @@ setup(
     entry_points={"console_scripts": ["nanoemoji=nanoemoji.nanoemoji:main",],},
     install_requires=[
         "absl-py>=0.9.0",
+        "CairoSVG>=2.4.2"
         "fs==2.4.11",
         "lxml>=4.0",
+        "nanosvg>=0.1",
         "regex>=2020.4.4",
         "skia-pathops>=0.3",
         "ufo2ft>=2.13.0",
         "ufoLib2>=0.6.2",
         # Horrible horrors lie here. Remove after merges.
         "fontTools @ https://github.com/fonttools/fonttools/tarball/otdata-colr",
-        "nanosvg @ https://github.com/rsheeter/nanosvg/tarball/master",
     ],
     # metadata to display on PyPI
     author="Rod S",
-    author_email="rsheeter",
-    description=("Exploratory utility for COLR fonts"),
+    author_email="rsheeter@google.com",
+    description=("Exploratory utility for color fonts"),
 )
