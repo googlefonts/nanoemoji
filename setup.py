a22402f0202d912445084081c47659afbedfad3b
Cosimo Lupo <clupo@google.com>
2020-04-17 18:39:27 +0100

    Update nanosvg and fonttools dependencies

--- setup.py
+++ setup.py
@@ -27,13 +27,12 @@ setup(
         "CairoSVG>=2.4.2",
         "fs==2.4.11",
         "lxml>=4.0",
-        "nanosvg>=0.1",
+        "nanosvg>=0.3.0",
         "regex>=2020.4.4",
         "skia-pathops>=0.3",
         "ufo2ft>=2.13.0",
         "ufoLib2>=0.6.2",
-        # Horrible horrors lie here. Remove after merges.
-        "fontTools @ https://github.com/fonttools/fonttools/tarball/otdata-colr",
+        "fontTools>=4.8.0",
     ],
     # metadata to display on PyPI
     author="Rod S",
