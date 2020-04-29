43d4dce9c47573e3922eac3d88c10741b923e691
Cosimo Lupo <clupo@google.com>
2020-04-29 10:41:17 +0100

    setup.py: require python >= 3.6; install dataclasses on < 3.7

--- setup.py
+++ setup.py
@@ -29,7 +29,9 @@ setup(
         "regex>=2020.4.4",
         "ufo2ft>=2.13.0",
         "ufoLib2>=0.6.2",
+        "dataclasses>=0.7; python_version < '3.7'",
     ],
+    python_requires=">=3.6",
     # metadata to display on PyPI
     author="Rod S",
     author_email="rsheeter@google.com",
