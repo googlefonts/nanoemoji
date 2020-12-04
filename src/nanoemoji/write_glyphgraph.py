c564be5900bc03abb96749cb07875bd8023ef3b5
rsheeter <rsheeter@google.com>
2020-12-03 23:02:01 -0800

    Restore glyphgraph to working order and make it easier to make for future format shifts

--- src/nanoemoji/write_glyphgraph.py
+++ src/nanoemoji/write_glyphgraph.py
@@ -21,6 +21,7 @@ from absl import flags
 from absl import logging
 from collections import Counter
 from fontTools import ttLib
+from fontTools.ttLib.tables import otTables as ot
 from graphviz import Digraph  # pytype: disable=import-error
 from lxml import etree  # pytype: disable=import-error
 from nanoemoji.colors import Color
@@ -112,9 +113,15 @@ def _color_line_node(palette, color_line):
 
 
 def _paint_node(glyph_order, palette, paint) -> Node:
-    if paint.Format == 1:
+    if paint.Format == ot.Paint.Format.PaintColrLayers:
+        return Node(
+            node_id=(
+                f"Layers_[{paint.FirstLayerIndex}..{paint.FirstLayerIndex+paint.NumLayers}]"
+            )
+        )
+    if paint.Format == ot.Paint.Format.PaintSolid:
         return _color_index_node(palette, paint.Color)
-    if paint.Format == 2:
+    if paint.Format == ot.Paint.Format.PaintLinearGradient:
         id_parts = (
             "Linear",
             f"p0({paint.x0.value}, {paint.y0.value})",
@@ -123,7 +130,7 @@ def _paint_node(glyph_order, palette, paint) -> Node:
             _color_line_node(palette, paint.ColorLine).node_id,
         )
         return Node(node_id="_".join(id_parts), node_label=" ".join(id_parts[:-1]))
-    if paint.Format == 3:
+    if paint.Format == ot.Paint.Format.PaintRadialGradient:
         id_parts = (
             "Radial",
             f"c0({paint.x0.value}, {paint.y0.value})",
@@ -133,7 +140,7 @@ def _paint_node(glyph_order, palette, paint) -> Node:
             _color_line_node(palette, paint.ColorLine).node_id,
         )
         return Node(node_id="_".join(id_parts), node_label=" ".join(id_parts[:-1]))
-    if paint.Format == 4:
+    if paint.Format == ot.Paint.Format.PaintGlyph:
         id_parts = (
             "Glyph",
             paint.Glyph,
@@ -143,15 +150,13 @@ def _paint_node(glyph_order, palette, paint) -> Node:
             node_id="_".join(id_parts),
             node_label=f"gid {glyph_order.index(paint.Glyph)}",
         )
-    if paint.Format == 5:
+    if paint.Format == ot.Paint.Format.PaintColrGlyph:
         id_parts = (
-            "Slice",
             "Base",
             paint.Glyph,
-            "%d..%d" % (paint.FirstLayerIndex, paint.LastLayerIndex),
         )
         return Node(node_id="_".join(id_parts))
-    if paint.Format == 6:
+    if paint.Format == ot.Paint.Format.PaintTransform:
         id_parts = (
             "Transform",
             f"î {paint.Transform.xx.value},{paint.Transform.xy.value}",
@@ -161,7 +166,7 @@ def _paint_node(glyph_order, palette, paint) -> Node:
             _paint_node(glyph_order, palette, paint.Paint).node_id,
         )
         return Node(node_id="_".join(id_parts))
-    if paint.Format == 7:
+    if paint.Format == ot.Paint.Format.PaintComposite:
         id_parts = (
             "Composite",
             _paint_node(glyph_order, palette, paint.SourcePaint).node_id,
@@ -169,12 +174,6 @@ def _paint_node(glyph_order, palette, paint) -> Node:
             _paint_node(glyph_order, palette, paint.BackdropPaint).node_id,
         )
         return Node(node_id="_".join(id_parts), node_label=paint.CompositeMode.name)
-    if paint.Format == 8:
-        return Node(
-            node_id=(
-                f"Layers_[{paint.FirstLayerIndex}..{paint.FirstLayerIndex+paint.NumLayers}]"
-            )
-        )
 
     raise NotImplementedError(f"id for format {paint.Format} ({dir(paint)})")
 
@@ -193,26 +192,33 @@ def _paint(dag, parent, font, paint, depth):
 
     # Descend
     if new_edge:
-        if paint.Format in (2, 3):
-            color_node = _color_line_node(palette, paint.ColorLine)
-            dag.graph.node(color_node.node_id, color_node.node_label)
-            dag.edge(node_id, color_node.node_id)
-        elif paint.Format == 4:
-            _paint(dag, node_id, font, paint.Paint, depth + 1)
-        # adding format 5 edges makes the result a horrible mess
-        # elif paint.Format == 5:
-        #   _glyph(dag, node_id, font, _only(_base_glyphs(font, lambda g: g.BaseGlyph == paint.Glyph)), depth + 1)
-        elif paint.Format == 6:
-            _paint(dag, node_id, font, paint.Paint, depth + 1)
-        elif paint.Format == 7:
-            _paint(dag, node_id, font, paint.BackdropPaint, depth + 1)
-            _paint(dag, node_id, font, paint.SourcePaint, depth + 1)
-        elif paint.Format == 8:
+        if paint.Format == ot.Paint.Format.PaintColrLayers:
             child_paints = font["COLR"].table.LayerV1List.Paint[
                 paint.FirstLayerIndex : paint.FirstLayerIndex + paint.NumLayers
             ]
             for child_paint in child_paints:
                 _paint(dag, node_id, font, child_paint, depth + 1)
+        elif paint.Format == ot.Paint.Format.PaintSolid:
+            pass
+        elif paint.Format in (
+            ot.Paint.Format.PaintLinearGradient,
+            ot.Paint.Format.PaintRadialGradient,
+        ):
+            color_node = _color_line_node(palette, paint.ColorLine)
+            dag.graph.node(color_node.node_id, color_node.node_label)
+            dag.edge(node_id, color_node.node_id)
+        elif paint.Format == ot.Paint.Format.PaintGlyph:
+            _paint(dag, node_id, font, paint.Paint, depth + 1)
+        # adding format 5 edges makes the result a horrible mess
+        # elif paint.Format == 5:
+        #   _glyph(dag, node_id, font, _only(_base_glyphs(font, lambda g: g.BaseGlyph == paint.Glyph)), depth + 1)
+        elif paint.Format == ot.Paint.Format.PaintTransform:
+            _paint(dag, node_id, font, paint.Paint, depth + 1)
+        elif paint.Format == ot.Paint.Format.PaintComposite:
+            _paint(dag, node_id, font, paint.BackdropPaint, depth + 1)
+            _paint(dag, node_id, font, paint.SourcePaint, depth + 1)
+        else:
+            raise ValueError(f"Unsupported: {paint.Format}")
 
 
 def _glyph(dag, parent, font, base_glyph, depth=0):
